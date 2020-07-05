80548b7ca5dcffb38dfb079a99967ecf77224057
Alexandros Tsichouridis <alexchih02@gmail.com>
2020-07-05 22:17:36 +0300

    initial commit

---
+++ studentsweb-backend/src/test/java/io/alexc/studentsweb/StudentsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alexc.studentsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
